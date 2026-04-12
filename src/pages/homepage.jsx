c35c774ddfb6b42ced946fffdd5739c4dc9a3eef
Dardeka <ddkwari2@gmail.com>
2026-04-12 13:37:34 +0700

    Update homepage layout and content; replace favicon and add logo image

--- src/pages/homepage.jsx
+++ src/pages/homepage.jsx
@@ -1,24 +1,27 @@
 function Homepage() {
     return(
         <>
-            <header className="flex flex-row bg-blue-500 text-white py-4 px-16">
-                <h1 className="text-2xl font-bold ">
-                    Pendidikan<br />Berkualitas
-                </h1>
-                <div className="ml-auto flex flex-row space-x-16">
-                    <button className="p-2 rounded-lg cursor-pointer hover:bg-blue-700 hover:shadow-md hover:scale-110 transition duration-200">Empati dan<br/>Penelitian</button>
-                    <button className="py-2 px-4 rounded-lg cursor-pointer hover:bg-blue-700 hover:shadow-md hover:scale-110 transition duration-200">Ide</button>    
-                    <button className="p-2 rounded-lg cursor-pointer hover:bg-blue-700 hover:shadow-md hover:scale-110 transition duration-200">Pembuatan Prototipe</button>
-                    <button className="p-2 rounded-lg cursor-pointer hover:bg-blue-700 hover:shadow-md hover:scale-110 transition duration-200">Pengujian dan<br/>Peninjauan</button>
-                    <button className="p-2 rounded-lg cursor-pointer hover:bg-blue-700 hover:shadow-md hover:scale-110 transition duration-200">Refleksi</button>
+            <header className="sticky top-0 flex flex-row bg-[#003366] text-white py-4 items-center shadow-xl/50 z-10">
+                <div className="flex flex-row items-center space-x-4 ml-16">
+                    <img src="/images/logo.png" alt="" className="w-20 h-20"/>
+                    <h1 className="text-xl font-bold ">
+                        Pendidikan<br />Berkualitas
+                    </h1>
+                </div>
+                <div className="ml-auto flex flex-row space-x-5 mr-10 font-semibold">
+                    <button className="h-12 px-4 rounded-lg cursor-pointer hover:bg-[#002244] hover:shadow-md hover:scale-110 transition duration-200">Empati & Penelitian</button>
+                    <button className="h-12 px-4 rounded-lg cursor-pointer hover:bg-[#002244] hover:shadow-md hover:scale-110 transition duration-200">Ide</button>    
+                    <button className="h-12 px-4 rounded-lg cursor-pointer hover:bg-[#002244] hover:shadow-md hover:scale-110 transition duration-200">Pembuatan Prototipe</button>
+                    <button className="h-12 px-4 rounded-lg cursor-pointer hover:bg-[#002244] hover:shadow-md hover:scale-110 transition duration-200">Pengujian dan Peninjauan</button>
+                    <button className="h-12 px-4 rounded-lg cursor-pointer hover:bg-[#002244] hover:shadow-md hover:scale-110 transition duration-200">Refleksi</button>
                 </div>
             </header>
             <main>
-                <section className="flex flex-col items-center justify-center h-screen bg-[url('/images/hero.jpg')] bg-[length:100%_100%] bg-center bg-no-repeat">
-                    <div className="w-screen h-screen bg-gray-200/50 bg-opacity-75 p-8 flex flex-col justify-center items-center">
-                        <h2 className="text-6xl font-bold mb-4 text-center">Indonesia Emas <br/> Perlu Dibangun Sejak Dini</h2>
-                        <p className="text-lg mb-8">Melihat capaian Indonesia dalam menggapai potensi terbesarnya melalui pendidikan.</p>
-                        <button className="w-[200px] bg-blue-500 text-white px-6 py-3 rounded-lg cursor-pointer hover:bg-blue-600 transition duration-300">Lihat Selengkapnya</button>
+                <section className="flex flex-col items-center justify-center h-[500px] bg-[url('/images/hero.jpg')] bg-[length:100%_100%] bg-cover bg-center bg-no-repeat">
+                    <div className="w-screen h-screen bg-black/50 text-white p-8 flex flex-col justify-center items-center">
+                        <h2 className="text-6xl font-bold mb-4 text-center">Transformasi Pendidikan Indonesia Melalui Data dan Aksi Nyata.</h2>
+                        <p className="text-lg mb-8">Memantau perkembangan SDG 4 secara transparan untuk memastikan standar kualitas pendidikan yang berkelanjutan.</p>
+                        <button className="w-[200px] bg-blue-500 px-6 py-3 rounded-lg cursor-pointer hover:bg-blue-600 transition duration-300">Lihat Selengkapnya</button>
                     </div>
                 </section>
             </main>
